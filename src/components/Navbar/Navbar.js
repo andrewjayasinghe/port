382e2c011567c958bc5afcd3e8363b41fc8e5502
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2023-03-26 19:57:07 -0700

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ import nse1 from "./../../assets/NSE_1_Certification.pdf"
 import nse3 from "./../../assets/NSE_3_Certificate.pdf"
 import aws from "./../../assets/134_3_2647869_1668723274_AWS_Course_Completion_Certificate.pdf"
 import cisa from "./../../assets/Cyber-P.pdf"
-
+import qualys from "./../../assets/VMDR_coursecompletion.pdf"
 
 
 
@@ -68,6 +68,9 @@ class Navbar extends Component {
 
         <ReactBootStrap.NavDropdown.Divider />
         <ReactBootStrap.NavDropdown.Item href={cisa} target="_blank" className="drop" >CISA Cert</ReactBootStrap.NavDropdown.Item>
+
+        <ReactBootStrap.NavDropdown.Divider />
+        <ReactBootStrap.NavDropdown.Item href={qualys} target="_blank" className="drop" >Qualys Cert</ReactBootStrap.NavDropdown.Item>
 
         <ReactBootStrap.NavDropdown.Divider />
         <ReactBootStrap.NavDropdown.Item href={aws} target="_blank" className="drop" >AWS Cloud Practitioner Essentials</ReactBootStrap.NavDropdown.Item>
